0f4270fd52ad08fd6269f0cafd926a98c425d273
DanielOrtega94 <danielortegacarcamo@gmail.com>
2018-05-31 14:50:59 -0400

    arreglos

--- tablero.py
+++ tablero.py
@@ -1,18 +1,5 @@
 import chess
 import funciones as f
-#import juego as j
-#import heuristicas as h
 
-
-#instancia el tablero
 board = chess.Board()
 f.juego(board)
-
-#print(board.result())
-# state.is_stalemate() and state.is_insufficient_material() and
-
-#print(board.is_insufficient_material()) 
-
-
-
-
